f1a54a7bdae96b2a92a882daf94bcc60c1648bc9
derekbt96 <derekbt@yahoo.com>
2020-04-01 12:16:46 -0400

    color_segment_final

---
+++ Project_3/GMM.py
@@ -0,0 +1,174 @@
+import cv2
+import numpy as np
+from scipy.stats import multivariate_normal
+
+
+class GMM(object):
+    
+    # For training
+    _MAX_ITERATIONS = 600
+    _EPSILON = 0.0001
+    
+    def __init__(self,
+                 k,
+                 means,
+                 covariances,
+                 weights,
+                 median=None,
+                 color=None,
+                 threshold=0.7,
+                 train=False,
+                 train_data=None):
+
+        self._k = k
+        self._threshold = threshold
+        self._color = color
+
+        if train:
+            self._train = train_data
+            self._e = self._EPSILON
+            self._num_samples = self._train.shape[0]
+
+            # Thetas
+            self._rs = np.zeros((self._num_samples, self._k))
+
+            # Evenly weighted to start
+            self._pis = [1./self._k] * self._k
+
+            # Generate random starting covariance matrices
+            self._sigmas = []
+            for i in range(self._k):
+                self._sigmas.append(gen_random_pos_def(3, 80))
+
+            # Choose random points from the training set for means
+            idxs = np.random.choice(self._num_samples, self._k, False)
+            self._means = np.array(self._train[idxs, :], dtype=np.float)
+
+            self.hooray, self.iters, self._means, self._covariances, self._weights = self._train_gmm()
+
+            # Now that the data has been fit, compute the likelihoods for the training data
+            sh = train_data.shape
+            new_data = np.reshape(train_data, (sh[0], 1, 3))
+            med = self.predict_proba(new_data)
+            # self._median = np.max(np.mean(med, axis=0))
+            self._median = np.max(np.mean(med, axis=1))
+
+        else:
+            self._means = means
+            self._covariances = covariances
+            self._weights = weights
+            self._median = median
+
+    def save_params(self, prefix):
+        np.save(prefix + "median.npy", self._median)
+        np.save(prefix + "mean.npy", self._means)
+        np.save(prefix + "covars.npy", self._covariances)
+        np.save(prefix + "weights.npy", self._weights)
+
+    def load_params(self, prefix):
+        self._median = np.load(prefix + "median.npy")
+        self._means = np.load(prefix + "mean.npy")
+        self._covariances = np.load(prefix + "covars.npy")
+        self._weights = np.load(prefix + "weights.npy")
+
+    def apply(self, img_s):
+        if self._color:
+            img_s = cv2.cvtColor(img_s, self._color)
+
+        size = img_s.shape
+        y = size[0]
+        x = size[1]
+
+        # Put all pixels in a row
+        pixels = x * y
+        test = np.reshape(img_s, (pixels, 3))
+
+        likelihoods = self.predict_proba(test)
+
+        # Reshape into image format
+        loglikes = np.sum(likelihoods, axis=1)
+        loglike_img = np.reshape(loglikes, (y, x))
+
+        # Find points above threshold
+        out_image = np.zeros((y, x), dtype=np.uint8)
+        out_image[loglike_img > self._median*self._threshold] = 255
+        return out_image
+
+    def predict_proba(self, data):
+        # Placeholders
+        likelihoods = np.zeros((data.shape[0], self._k))
+
+        # Compute likelihoods
+        for i in range(self._k):
+            normal = multivariate_normal.pdf(data,
+                                             self._means[i],
+                                             self._covariances[i],
+                                             allow_singular=True)
+            pd = self._weights[i] * normal
+            likelihoods[:, i] = pd
+        return likelihoods
+
+    def _train_gmm(self):
+
+        # https://cmsc426spring2019.github.io/colorseg/
+
+        iter = 0
+        last_log_like = 0.
+        print "Starting gmm training..."
+        while True:
+            iter += 1
+
+            # Compute pdfs for each mean and covariance
+            for i in range(self._k):
+                # Using scipy to compute gaussian probs for training data, for some reason need to
+                # allow for singular covariances and it seems to work
+                # Posterior Distribution using Bayes Rule
+                normal = multivariate_normal.pdf(self._train,
+                                                 self._means[i],
+                                                 self._sigmas[i],
+                                                 allow_singular=True)
+
+                # Adjust by weight
+                pd = self._pis[i] * normal
+
+                # Update
+                self._rs[:, i] = pd
+
+            # Compute log likelihood
+            tmp = np.sum(self._rs, axis=1)
+            log_like = np.sum(np.log(tmp))
+
+            # Have we converged?
+            if iter > 1:
+                print "iteration: %s     ||mu - mu_1|| = %s" % (iter, log_like - last_log_like)
+                if np.abs(log_like - last_log_like) < self._e:
+                    print "We did it!"
+                    hooray = True
+                    break
+
+            # Have we failed?
+            if iter == self._MAX_ITERATIONS:
+                print "We didn't do it :("
+                hooray = False
+                break
+
+            # Add the new likelihoods
+            last_log_like = log_like
+
+            # Iterate on means and covariances
+            denom = np.sum(self._rs, axis=1)
+            self._rs = (self._rs.T / denom).T
+            nks = np.sum(self._rs, axis=0)
+            for i in range(self._k):
+                # Update weights
+                self._pis[i] = 1. / self._num_samples * nks[i]
+
+                # Mean
+                self._means[i] = 1. / nks[i] * np.sum(self._rs[:, i] * np.transpose(self._train), axis=1)
+
+                # Covariance
+                tmp = np.matrix(self._train - self._means[i])
+                self._sigmas[i] = np.array(1 / nks[i] * np.dot(np.multiply(tmp.T, self._rs[:, i]), tmp))
+
+        # Return success, means, covars, and weights
+        return hooray, iter, self._means, self._sigmas, self._pis
--- Project_3/gmm_trainer.py
+++ Project_3/gmm_trainer.py
@@ -12,136 +12,77 @@ def main():
 	capture = cv2.VideoCapture('detectbuoy.avi')
 	capture_mask = cv2.VideoCapture('buoy_mask.avi')
 
-	color_seg = gmm()
 	mask_gen = color_mask()
+
+	k = 2
+	color = "HSV"
+	cv2_color = cv2.COLOR_BGR2HSV
+
+	gmm = GMM(
+    k, 
+    [], [], [], # Loaded from config
+    threshold=0.1,
+    color=cv2_color
+    )
+	gmm.load_params(prefix)
+	
 	frame_indx = 0
 	while(True):
 
 		
 		ret, frame = capture.read()
-		ret, frame_mask = capture_mask.read()
+		
 		frame_indx += 1
 		if frame is None:
 			break
 
-
-		# mask1 = cv2.inRange(frame_mask, (20,0,0), (255,0,0))
-		# mask2 = cv2.inRange(frame_mask, (0,20,0), (0,255,0))
-
-
-		# (mask1, mask2, mask3) = cv2.split(frame_mask)
-		
-		# kernel = np.ones((9,9),np.uint8)
-
-		# mask1 = cv2.inRange(frame_mask, (0,0,225), (10,10,255))
-		# mask1 = cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, kernel)
-
-		# mask2 = cv2.inRange(frame_mask, (0,50,0), (10,255,10))
-		# mask2 = cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, kernel)
-
-		# mask3 = cv2.inRange(frame_mask, (50,0,0), (255,10,10))
-		# mask3 = cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, kernel)
-
-		# mask_full = cv2.bitwise_or(mask1, cv2.bitwise_or(mask2, mask3))
-
-		mask1 = mask_gen.get_mask(frame,frame_indx,1)
-		mask2 = mask_gen.get_mask(frame,frame_indx,2)
-		mask3 = mask_gen.get_mask(frame,frame_indx,3)
-		
-		result = cv2.merge([mask1,mask2,mask3])
 		
 
-		# result = cv2.bitwise_and(frame,frame, mask = mask1)
-		
-		
-		
-		# mask2 = mask2 > 200
-		# mask3 = mask3 > 200
-		# print('2')
-		# frame[mask1] == (255,0,0)
-		# frame[mask2] == (0,255,0)
-		# frame[mask3] == (0,0,255)
-		# print('3')
-		
-		# color_seg.spin(frame)
+		mask = mask_gen.get_mask()
+
+		color_seg.spin(frame)
+
 
 		# cv2.imshow('mask1',mask1)
 		# cv2.imshow('mask2',mask2)
 		# cv2.imshow('mask3',mask3)
-		cv2.imshow('result',result)
+		cv2.imshow('result',mask)
 		
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 		
 	capture.release()
-	capture_mask.release()
+	mask_gen.cap.release()
 	cv2.destroyAllWindows()
 
 
 
 class color_mask:
 	def __init__(self):
-		temp = loadmat('vid_points.mat')
-		self.pnts = temp['image_points']
-		
-		self.wnd = 25
-		self.thres = [80,80,40]
-		
+		self.cap = cv2.VideoCapture('buoy_mask.avi')
+		self.thres = [127,255]
+		self.kernel3 = np.ones((3,3),np.uint8)
+		self.kernel5 = np.ones((5,5),np.uint8)
 
-	def get_mask(self,img,img_num,mask_num):
-		x = self.pnts[img_num,((mask_num-1)*2)]
-		y = self.pnts[img_num,((mask_num-1)*2)+1]
-		
-		color = img[x,y,:]
+	def get_mask(self):
+		ret, frame_mask = self.cap.read()
 
-		temp_wnd = self.wnd
-		if img_num > 135:
-			temp_wnd = 40
-		
-		img_red = img[max(x-temp_wnd,0):min(x+temp_wnd,640),max(y-temp_wnd,0):min(y+temp_wnd,480),:]
-		
+		if frame_mask is None:
+			return None
 
-		upper_b = (color[0]-self.thres[0],color[1]-self.thres[1],color[2]-self.thres[2])
-		lower_b = (color[0]+self.thres[0],color[1]+self.thres[1],color[2]+self.thres[2])
-		print(upper_b)
-		print(lower_b)
-		mask = cv2.inRange(img_red, lower_b, upper_b)
-		
-		mask_final = np.zeros((480,640),np.uint8)
-		
-		mask_final[max(x-wnd,0):min(x+wnd,640),max(y-wnd,0):min(y+wnd,480)] = mask
-		
-		return mask_final
+		ret,thres = cv2.threshold(frame_mask,self.thres[0],self.thres[1],cv2.THRESH_BINARY)
+		(mask1, mask2, mask3) = cv2.split(thres)
 
+		mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, self.kernel3)
+		mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, self.kernel3)
+		mask3 = cv2.morphologyEx(mask3, cv2.MORPH_OPEN, self.kernel3)
 
+		mask1 = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, self.kernel5)
+		mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, self.kernel5)
+		mask3 = cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, self.kernel5)
 
+		return cv2.merge([mask1,mask2,mask3])
 
-
-
-
-class gmm:
-	def __init__(self):
-		self.K =  np.array([[  1.15422732e+03,   0.00000000e+00,   6.71627794e+02],
-					[  0.00000000e+00,   1.14818221e+03,   3.86046312e+02],
-					[  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]])
-		self.dist = np.array([ -2.42565104e-01,  -4.77893070e-02,  -1.31388084e-03,  -8.79107779e-05, 2.20573263e-02])
-
-		self.newcameramtx, self.roi = cv2.getOptimalNewCameraMatrix(self.K,self.dist,(1280,720),1, (1280,720))
-		
-		
-		
-
-	def spin(self,img):
-		
-		return img
-
-		
-		
-	def change_colorspace(self,img):
-		HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-		LAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-		YCr = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-		return HSV
 
 main()
 
