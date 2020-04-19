c547f05ce543de50beb9a3260575c2f8d9e8ed01
derekbt96 <derekbt@yahoo.com>
2020-04-18 20:32:17 -0400

    LK get start bounds

--- Project_4/Code/functions.py
+++ Project_4/Code/functions.py
@@ -5,17 +5,38 @@ from matplotlib import pyplot as plt
 
 
 class LK_tracker:
-    def __init__(self):
-        self.stuff = None
+    def __init__(self,bounds=None):
+        self.bounds = []
             
-
+        
     def apply(self,img):
-
+        if len(self.bounds) == 0:
+            self.get_start_bound(img)
+            
         out_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+        
         return out_image
 
 
+    def bound_callback(self,event,x,y,flags,param):
+        # print(event,cv2.EVENT_LBUTTONDBLCLK)
+        if event == 4:
+            if len(self.bounds) == 0:
+                self.bounds = [x,y]
+            elif len(self.bounds) == 2:
+                self.bounds.extend([x,y])
+
+    def get_start_bound(self,img):
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image',self.bound_callback)
+        cv2.imshow('image',img)
+
+        while len(self.bounds) < 4:
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+
+        
 
 class get_frames:
     def __init__(self,video_seq):
@@ -30,7 +51,6 @@ class get_frames:
             self.file_route = 'DragonBaby/img/'
 
             
-
     def get_next_frame(self):
         num = str(self.frame_num)
         num = num.zfill(4)
