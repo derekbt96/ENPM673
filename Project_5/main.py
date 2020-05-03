4ec7425d3616ff1852676cfde8f3af66dfe49f7f
Surabhi Verma <surabhiverma96@gmail.com>
2020-05-03 12:36:23 -0400

    got the final R|t with linear triangulation

--- Project_5/main.py
+++ Project_5/main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from ReadCameraModel import ReadCameraModel
 from UndistortImage import UndistortImage
 import os
-from functions import RansacFundamental, EpipolarLines, getCameraPose, camera_movement
+from functions import RansacFundamental, EpipolarLines, getCameraPose, Linear,checkCheirality 
 dirpath = os.getcwd()
 
 fx ,fy ,cx ,cy ,G_camera_image, LUT = ReadCameraModel('./model')
@@ -25,7 +25,9 @@ orb = cv2.ORB_create(nfeatures=400,patchSize=51)
 # orb = cv2.SIFT_create(nfeatures=400)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
             
-pose = camera_movement()
+current_pnt = np.zeros((3,1))
+current_R = np.identity(3)
+all_points = np.zeros((1,3))
 
 for subdir, dirs, files in os.walk(dirpath + '/stereo/centre'):
     files.sort()
@@ -72,31 +74,66 @@ for subdir, dirs, files in os.walk(dirpath + '/stereo/centre'):
             img_orig1 = img_orig2
             orb_kp1 = kp2
             orb_des1 = des2
-            
-
-            # cv2.imshow('Matches', match_img)
-            # cv2.waitKey(100)
 
             points_f1 = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
             points_f2 = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+
+            l = len(points_f2)
+    
+            points_f1 = np.reshape(points_f1[:,0], (l,2))
+            points_f2 = np.reshape(points_f2[:,0], (l,2))
+
+            # Change to homogeneous coordinates
+            points_f1 = np.hstack((points_f1, np.ones((l,1))))
+            points_f2 = np.hstack((points_f2, np.ones((l,1))))
 
             F, inliers_f1, inliers_f2 = RansacFundamental(points_f1, points_f2)
             
             # draw epipolar lines
             img_f1, img_f2 = EpipolarLines(img_orig1, inliers_f1, img_orig2, inliers_f2, F)
             
-            # Compute essential matrix
-            t,R = getCameraPose(F, K, points_f1, points_f2)
+            # These are the possible transforms between frame1 (f1) and frame2 (f2)
+            T = getCameraPose(F, K, points_f1, points_f2)
+            # X: 4 sets of 3D points (from triangulation) from 4 camera poses
+            X = np.zeros((4,l,3))
+
+            T_1 = np.identity(4)[:3,:] # 3x4
+
+            for i in range(4):
+                T_2 = T[0,:,:]
+                X[i,:,:] = Linear(K, T_1, T_2, points_f1, points_f2)
+            # R = T[:,:,:3]
+            # t = T[:,:,3]
+            # print(np.shape(R))
+            # print(np.shape(t))
+            # print(R)
+            # print(t)
+
+            # Final transform and points in front of camera
+            R_final, t_final, X_final = checkCheirality(T, X)
+            # print(R_final)
+            # print(t_final)
+            # print(np.shape(X_final))
 
             # Log camera movement
-            pose.update_pos(R,t)
-            
+            # Calculate pose of the camera relative to the first camera pose
+            # This essentially is Visual Odometry!
+            ############
+            # NEED TO DO THIS NOW!! :'(
+            ############ 
+            # current_R = np.matmul(R,current_R)
+            # current_pnt = np.hstack(current_pnt) + np.matmul(current_R,t)
+            # all_points = np.vstack([all_points,current_pnt])
+       
 
             # break
-            cv2.imshow('Epipolar lines', img_f1)
-            # cv2.imshow('Epipolar lines', match_img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # cv2.imshow('Epipolar lines', img_f1)
+            # # cv2.imshow('Epipolar lines', match_img)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
 
 
-# pose.plot()
+plt.figure(figsize=(6,6))
+plt.plot(all_points[:,0], all_points[:,2])
+plt.grid(True)
+plt.show()
